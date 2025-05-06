320e20e99f065456cc2680b1108f26eb865f276c
Amrith Gandham <amrithgandham@gmail.com>
2025-05-05 17:08:39 -0700

    Initial Commit

---
+++ iQuiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iQuiz
+//
+//  Created by Amrith Gandham on 5/5/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
